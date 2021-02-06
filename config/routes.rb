37aa2c7a20ee9c3e31b2c6f3cbb01fe0e2634862
kai3tctm <get_wild10run@yahoo.co.jp>
2021-02-06 21:02:15 +0900

    プルダウンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'messages/index'
   root to: "messages#index"
   resources :rooms, only: [:new, :create]
+  resources :users, only: [:edit, :update]
 end
